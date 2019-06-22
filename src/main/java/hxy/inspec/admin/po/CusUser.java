c1e7338573dc58d974f94f90b022635898ec1b54
aohanhongzhi <aohanhongzhi@qq.com>
2019-06-22 16:06:00 +0800

    first commit for team

--- src/main/java/hxy/inspec/admin/po/CusUser.java
+++ src/main/java/hxy/inspec/admin/po/CusUser.java
@@ -10,14 +10,13 @@ public class CusUser {
 	private String cuspasswd;
 	private String country;
 	private String city;
-	private String cusgrade;//等级，与信用相关
+	private String cusgrade;// 等级，与信用相关
 	private String cusvip;
 	private String cusaddress;// 地址
 	private String cusdate;// 注册日期
-	private String custrade;//行业
-	private String email;
-	private String cusMoney;//钱包余额
-	private String cusTempMoney;//钱包变化后的余额。充值用临时余额冲。
-	private String cusOrders;//订单数
-
+	private String custrade;// 行业
+	private String email;//
+	private String cusMoney;// 钱包余额
+	private String cusTempMoney;// 钱包变化后的余额。充值用临时余额冲。
+	private String cusOrders;// 订单数
 }
